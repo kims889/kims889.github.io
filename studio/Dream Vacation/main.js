91d90e3dfb8ab1aa157e40e528337c1cb6dd8ec7
kims889 <kims889@newschool.edu>
2018-05-11 08:35:09 -0400

    update

--- studio/Dream Vacation/main.js
+++ studio/Dream Vacation/main.js
@@ -1,51 +1,49 @@
-     $('.album').onclick(function() {
-  var album = $(this);
-  var audio = album.find('audio').get(0);
- 
-  audio.play();
+var audio;
+
+$(function() {
+  $('.album').click(function() {
+    var album = $(this);
+    audio = album.find('audio').get(0);
+
+    $('.album audio').each(function() {
+      $(this).get(0).pause();
+    });
+
+    audio.play();
+    $(".play-button-img").addClass("stopped");
+  });
+
+  // slider controller
+  // adapted from 
+  // https://codepen.io/anon/pen/RyQxdv
+  const metronome = $('#metronome');
+  const input = $("#metronome input");
+  const bpm = $('#bpm');
+
+  let speed = 0;
+  var $element = $('.element');
+
+  function handleUpdate(e) {
+    speed = 60 / this.value;
+    document.documentElement.style.setProperty(`--speed`, speed + 's');
+    bpm.text(this.value);
+    $element.css('animation-duration', speed / 100 + 's');
+    audio.playbackRate = this.value;
+  }
+
+  input.on('change', handleUpdate);
+  input.on('mousemove', handleUpdate);
+
+  $("#play-button").on('click', function() {
+    if ($(".play-button-img").hasClass("stopped")) {
+      audio.pause(); 
+      $(".play-button-img").removeClass("stopped")
+    }
+    else {
+      audio.play(); 
+      $(".play-button-img").addClass("stopped")
+    }
+
+  });
+
 });
-
-
-// document.getElementById("demo").onclick = function() {myFunction()};
-
-$('.play-button').click(function() {
-  playAudio();
-});
-
-var audio = document.getElementById("myAudio"); 
-
-function playAud() { 
-    audio.play(); 
-} 
-
-function pauseAud() { 
-    audio.pause(); 
-}
-
-// Metronome bpm
-/*
-class Metronome extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      playing: false,
-      count: 0,
-      bpm: 100,
-      beatsPerMeasure: 4
-    };
-  }*/
-
-// Volume function
-/*
-function getVolume() { 
-    alert(aud.volume);
-} 
-  
-function setHalfVolume() { 
-    aud.volume = 0.2;
-} 
-  
-function setFullVolume() { 
-    pauseAudio.volume = 1.0;
-} */ 
